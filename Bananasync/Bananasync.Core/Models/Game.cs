f34b4eb9443eae08868b07dcb611d283003deb0d
xeronik <xeronik@emulate.cc>
2016-04-29 22:05:05 +0200

    POCO for games

---
+++ Bananasync/Bananasync.Core/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Bananasync.Core.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title of the game e.g. "Minecraft"
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Path to the logo of the game
+        /// </summary>
+        public string ImagePath { get; set; }
+
+        /// <summary>
+        /// Path to all savegame folders that should be synced
+        /// </summary>
+        public string DataPath { get; set; }
+    }
+}
