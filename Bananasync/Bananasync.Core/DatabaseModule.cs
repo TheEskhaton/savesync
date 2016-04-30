1428db8e6b33dff8579a1ca5cd41f760576f384c
xeronik <xeronik@emulate.cc>
2016-04-30 15:46:54 +0200

    Using DI for IDbConnection

---
+++ Bananasync/Bananasync.Core/DatabaseModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using Ninject.Modules;
+
+namespace Bananasync.Core
+{
+    class DatabaseModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IDbConnection>().To<SQLiteConnection>();
+        }
+    }
+}
