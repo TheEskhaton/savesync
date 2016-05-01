73d422e358fbac7a4b5501b33f4f27a1f0dccff6
unknown <theeskhaton@gmail.com>
2016-05-01 10:34:11 +0200

    Staged deletes, fixed version mismatch

---
+++ Bananasync/Bananasync.Core/Repositories/GameRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bananasync.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bananasync.Core.Repositories
+{
+    public class GameRepository : Repository<Game>
+    {
+    }
+}
